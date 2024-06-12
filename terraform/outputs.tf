cf1a6451b029da15f85df6649b1ae63211b3925b
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-06-12 15:34:45 +1200

    updated outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,12 +2,20 @@ output "source_kms_key_arn" {
   value = module.kms-source.kms_key_arn
 }
 
-output "destination_kms_key_arn" {
-  value = module.kms-dest.kms_key_arn
+output "source_bucket_name" {
+  value = module.s3-source.bucket_name
 }
 
 output "source_bucket_arn" {
   value = module.s3-source.bucket_arn
+}
+
+output "destination_kms_key_arn" {
+  value = module.kms-dest.kms_key_arn
+}
+
+output "destination_bucket_name" {
+  value = module.s3-dest.bucket_name
 }
 
 output "destination_bucket_arn" {
