1cdbbe3761f5a669b1b0d3e46d025b48a15f5381
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-06-17 09:10:58 +1200

    updates

---
+++ labfiles/outputs.tf
@@ -0,0 +1,7 @@
+output "instance_id_region1" {
+  value = aws_instance.region1.id
+}
+
+output "instance_id_region2" {
+  value = aws_instance.region2.id
+}
--- labfiles/vars.tf
+++ labfiles/vars.tf
@@ -1,4 +1,5 @@
 variable "s3_mrap_arn" {
   description = "S3 multi-region access point ARN"
   type        = string
+  default     = "ARN_NOT_SET"
 }
